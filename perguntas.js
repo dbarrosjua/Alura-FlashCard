29049a2c305662d31a020937deba44557cc28c66
dbarrosjua <168599664+dbarrosjua@users.noreply.github.com>
2024-11-24 20:23:15 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é x e y sabendo que: x+y=5 / 3x-y=11?',
+    'S=(4,1)'
+)
+
+criaCartao(
+    'Química',
+    'O que é quiímica orgânica?',
+    'A parte da química que estuda as ligações do Carbono'
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual a teoria de Ulrick Beck?',
+    'Teoria dos Riscos criados'
+)
+
+criaCartao(
+    'Portugês',
+    'Qual a primeira fase do Romantismo e qual seu principal autor?',
+    'Nacionalismo, com Gonçalves Dias como principal autor'
+)
